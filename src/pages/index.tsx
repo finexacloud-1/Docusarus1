4d0ca5cdcec08421768270420c18ed924ec2d7cc
origene01 <origeneexploral@gmail.com>
2026-06-23 16:32:48 -0400

    Holamundo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,8 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+            Hola mundp ⏱️
+          </Link> 
         </div>
       </div>
     </header>
